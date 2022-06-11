9f7a4e229c288f00b4c2e23fe2f2455315aeb495
Bora Guven <guvenbe@gmail.com>
2022-06-10 20:34:08 -0400

    First end point

--- src/main/java/com/amigos/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/amigos/springbootdemo/SpringbootdemoApplication.java
@@ -2,12 +2,22 @@ package com.amigos.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringbootdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+    @GetMapping
+    public List<String> hello(){
+        return List.of("Hello", " World");
+    }
 
 }
