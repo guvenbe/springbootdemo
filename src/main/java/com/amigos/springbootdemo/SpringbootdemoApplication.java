278f8f9d57832aecc6359049236effffe11e7b32
Bora Guven <guvenbe@gmail.com>
2022-06-10 21:00:05 -0400

    student class

--- src/main/java/com/amigos/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/amigos/springbootdemo/SpringbootdemoApplication.java
@@ -1,10 +1,13 @@
 package com.amigos.springbootdemo;
 
+import com.amigos.springbootdemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,17 @@ public class SpringbootdemoApplication {
     }
 
     @GetMapping
-    public List<String> hello(){
-        return List.of("Hello", " World");
+    public List<Student> hello(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(1966, Month.FEBRUARY,5),
+                        21
+
+                )
+        );
     }
 
 }
