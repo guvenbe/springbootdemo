0e0493d1fbe585364d77958e85bc39a65710d96b
Bora Guven <guvenbe@gmail.com>
2022-06-17 03:05:52 -0400

    commadlineRunner

--- src/main/java/com/amigos/springbootdemo/student/StudentConfig.java
+++ src/main/java/com/amigos/springbootdemo/student/StudentConfig.java
@@ -1,12 +1,36 @@
 package com.amigos.springbootdemo.student;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.FEBRUARY;
 
 @Configuration
 public class StudentConfig {
-    CommandLineRunner commandLineRunner(StudentRepository repository){
-        return args->{
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam.jamal@gmail.com",
+                    LocalDate.of(1969, FEBRUARY, 5),
+                    22
+            );
+
+            Student alex = new Student(
+                    "alex",
+                    "alex.gone@gmail.com",
+                    LocalDate.of(1989, FEBRUARY, 5),
+                    22
+            );
+
+            repository.saveAll(List.of(mariam, alex));
 
         };
 
