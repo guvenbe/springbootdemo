2ce51f2b0311c6004b75276c62de11ec74b25337
Bora Guven <guvenbe@gmail.com>
2022-06-17 18:08:11 -0400

    delete student

--- src/main/java/com/amigos/springbootdemo/student/StudentService.java
+++ src/main/java/com/amigos/springbootdemo/student/StudentService.java
@@ -34,4 +34,15 @@ public class StudentService {
         log.info("**************Adding Student:\n" + student);
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if(!exists){
+                throw new IllegalStateException(
+                        "student with id " + studentId + " does not exists"
+                );
+        }
+        studentRepository.deleteById(studentId);
+
+    }
 }
